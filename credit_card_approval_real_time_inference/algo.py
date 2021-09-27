93dc797fa8ae405f2a80b487c34ecd94edbee449
Zhilei Ma <80358105+zmaalgorithmia@users.noreply.github.com>
2021-09-27 08:16:24 -0400

    initial deployment

--- credit_card_approval_real_time_inference/algo.py
+++ credit_card_approval_real_time_inference/algo.py
@@ -1,18 +1,18 @@
 import Algorithmia
-import os
 import numpy as np
 import time
 from joblib import load
-
-# DEBUG
-import logging
 
 
 client = Algorithmia.client()
 
 # Model version A - Gradient boosting classifier
+# model = load(client.file(
+#     "data://zma/credit_card_approval/model-a.joblib").getFile().name)
+
+# Model version B - Random forrest classifier
 model = load(client.file(
-    "data://zma/credit_card_approval/model-a.joblib").getFile().name)
+    "data://zma/credit_card_approval/model-b.joblib").getFile().name)
 
 
 def apply(input):
